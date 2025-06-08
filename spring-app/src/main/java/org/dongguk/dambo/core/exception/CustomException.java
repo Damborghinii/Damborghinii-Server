f4eb79b42d9bfcb1cba78c84f19fe20a3887c67b
Jang99u <alswkdrb@dgu.ac.kr>
2025-06-08 19:37:37 +0900

    feat: 표준 응답 포맷 및 커스텀 예외 구현

---
+++ spring-app/src/main/java/org/dongguk/dambo/core/exception/CustomException.java
@@ -0,0 +1,17 @@
+package org.dongguk.dambo.core.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public static CustomException type(ErrorCode errorCode) {
+        return new CustomException(errorCode);
+    }
+}
