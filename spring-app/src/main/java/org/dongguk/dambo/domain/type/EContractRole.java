7f03406e60cc2d1412ba06dc74f6339a50f8b601
ssangbaam <tkdqja2196@naver.com>
2025-06-11 21:09:23 +0900

    refac: implement layer 분리에 따른 리팩토링

--- spring-app/src/main/java/org/dongguk/dambo/domain/type/EContractRole.java
+++ spring-app/src/main/java/org/dongguk/dambo/domain/type/EContractRole.java
@@ -2,6 +2,8 @@ package org.dongguk.dambo.domain.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.dongguk.dambo.core.exception.CustomException;
+import org.dongguk.dambo.domain.exception.usercontract.UserContractErrorCode;
 
 @RequiredArgsConstructor
 @Getter
@@ -10,4 +12,12 @@ public enum EContractRole {
     LENDER("투자자");
 
     private final String label;
+
+    public static EContractRole convertToContractRole(String role) {
+        return switch (role) {
+            case "BORROWER" -> BORROWER;
+            case "LENDER" -> LENDER;
+            default -> throw new CustomException(UserContractErrorCode.INVALID_USER_CONTRACT_ROLE);
+        };
+    }
 }
--- spring-app/src/main/java/org/dongguk/dambo/domain/type/EContractStatus.java
+++ spring-app/src/main/java/org/dongguk/dambo/domain/type/EContractStatus.java
@@ -2,6 +2,10 @@ package org.dongguk.dambo.domain.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.dongguk.dambo.core.exception.CustomException;
+import org.dongguk.dambo.domain.exception.usercontract.UserContractErrorCode;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
@@ -12,4 +16,13 @@ public enum EContractStatus {
     COMPLETED("계약완료");
 
     private final String label;
+
+    public static List<EContractStatus> convertToEContractStatus(String status) {
+        return switch (status) {
+            case "ALL" -> List.of(INVESTING, MATCHED);
+            case "INVESTING" -> List.of(INVESTING);
+            case "MATCHED" -> List.of(MATCHED);
+            default -> throw new CustomException(UserContractErrorCode.INVALID_CONTRACT_STATUS);
+        };
+    }
 }
