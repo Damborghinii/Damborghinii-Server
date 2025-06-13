026e7b19234d871862363ed6ba7328eb4fbcaef7
ssangbaam <tkdqja2196@naver.com>
2025-06-14 04:01:05 +0900

    refac: LoanConstants로 클래스명 변경에 따른 리팩토링

--- spring-app/src/main/java/org/dongguk/dambo/constant/LoanConstants.java
+++ spring-app/src/main/java/org/dongguk/dambo/constant/LoanConstants.java
@@ -1,6 +1,6 @@
 package org.dongguk.dambo.constant;
 
-public class Constants {
+public class LoanConstants {
     public static Long EthereumMarketPrice = 3_800_000L;
     public static Float LoanToValue = 0.3F;
 }
