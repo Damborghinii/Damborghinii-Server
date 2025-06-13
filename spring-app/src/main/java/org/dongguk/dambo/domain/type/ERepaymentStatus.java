c32872d6dfd7c9aa23363ce05c3db9f6fe49f403
ssangbaam <tkdqja2196@naver.com>
2025-06-14 04:06:09 +0900

    refac: 상환 상태값 이름 변경

--- spring-app/src/main/java/org/dongguk/dambo/domain/type/ERepaymentStatus.java
+++ spring-app/src/main/java/org/dongguk/dambo/domain/type/ERepaymentStatus.java
@@ -2,13 +2,24 @@ package org.dongguk.dambo.domain.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.dongguk.dambo.core.exception.CustomException;
+import org.dongguk.dambo.domain.exception.usercontract.UserContractErrorCode;
 
 @RequiredArgsConstructor
 @Getter
 public enum ERepaymentStatus {
-    ON_TIME("정상 상환"),
-    LATE("연체"),
-    UNPAID("미납");
+    UPCOMING("상환예정"),
+    OVERDUE("미상환"),
+    REPAID("상환완료");
 
     private final String label;
+
+    public static ERepaymentStatus convertToERepaymentStatus(String status) {
+        return switch (status) {
+            case "UPCOMING" -> UPCOMING;
+            case "OVERDUE" -> OVERDUE;
+            case "REPAID" -> REPAID;
+            default -> throw new CustomException(UserContractErrorCode.INVALID_CONTRACT_STATUS);
+        };
+    }
 }
