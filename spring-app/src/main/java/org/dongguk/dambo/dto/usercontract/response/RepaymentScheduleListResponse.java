88891b88bbbcb3d01e65790d42ef9fc1e49cc355
ssangbaam <tkdqja2196@naver.com>
2025-06-15 18:17:03 +0900

    refac: 전체 정산관리 조회 dto 필드 타입 수정

--- spring-app/src/main/java/org/dongguk/dambo/dto/usercontract/response/RepaymentScheduleListResponse.java
+++ spring-app/src/main/java/org/dongguk/dambo/dto/usercontract/response/RepaymentScheduleListResponse.java
@@ -1,8 +1,11 @@
 package org.dongguk.dambo.dto.usercontract.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record RepaymentScheduleListResponse(
+        @JsonProperty("repaymentScheduleList")
         List<RepaymentScheduleResponse> repaymentScheduleResponseList
 ) {
         public static RepaymentScheduleListResponse from(
