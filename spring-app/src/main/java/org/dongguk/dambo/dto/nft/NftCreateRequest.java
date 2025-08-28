8c74cc68134315b0fabd8d5539bca4165f1780bf
Jang99u <alswkdrb@dgu.ac.kr>
2025-08-28 15:28:18 +0900

    hotfix: 명세와 다른 request 레코드 수정

--- spring-app/src/main/java/org/dongguk/dambo/dto/nft/NftCreateRequest.java
+++ spring-app/src/main/java/org/dongguk/dambo/dto/nft/NftCreateRequest.java
@@ -3,7 +3,6 @@ package org.dongguk.dambo.dto.nft;
 import java.util.List;
 
 public record NftCreateRequest(
-        String nftName,
         String title,
         List<String> singers,
         List<String> composers,
